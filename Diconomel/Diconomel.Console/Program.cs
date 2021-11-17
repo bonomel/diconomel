9f5e0253d8cb786f2d557ebb8ed78eff1aa16fd9
bonomel <m.vlasblom@gmail.com>
2021-11-17 10:56:23 +0100

    Created solution file.

---
+++ Diconomel/Diconomel.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diconomel.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
